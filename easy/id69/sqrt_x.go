122a5525961394cbc7f6847eafba50dbed0b0a90
oliver ding <oliverdding@outlook.com>
2026-06-27 22:57:40 +0800

    update id69

--- easy/id69/sqrt_x.go
+++ easy/id69/sqrt_x.go
@@ -8,18 +8,16 @@ package id69
 
 // @lc code=start
 func mySqrt(x int) int {
-	low, high := 1, x
-	for low <= high {
-		mid := low + (high-low)/2
-		if mid*mid > x {
-			high = mid - 1
-		} else if mid*mid < x {
-			low = mid + 1
+	lo, hi := 1, x+1
+	for lo < hi {
+		mid := lo + (hi-lo)>>1
+		if mid > x/mid {
+			hi = mid
 		} else {
-			return mid
+			lo = mid + 1
 		}
 	}
-	return high
+	return lo - 1
 }
 
 // @lc code=end
